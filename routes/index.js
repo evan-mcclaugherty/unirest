5cea0b1e7179a72466e3b299ff5ad7ce99c6a7bc
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-06-09 08:42:46 -0600

    add api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
 });
+router.get('/books', (req, res) => {
+    unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + process.env.NYT_API_KEY)
+        .end(response => {
+            console.log(response.body);
+        })
+        res.end("works bitch!");
+})
 
 module.exports = router;
